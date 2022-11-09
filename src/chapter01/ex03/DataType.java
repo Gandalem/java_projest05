7866c3384ca5e2eed178f8073c7835b1a0d4c732
gyumin park <rbals8429@gmail.com>
2022-11-09 14:56:40 +0900

    2022/11/09 오늘배운 데이터타입

---
+++ src/chapter01/ex03/DataType.java
@@ -0,0 +1,22 @@
+package chapter01.ex03;
+
+public class DataType {
+
+	public static void main(String[] args) {
+		
+		int a;
+		a = 10;
+		System.out.println(a);
+		
+		float f; 
+		f = 10;
+		
+		System.out.println(f);
+		
+		double d;
+		d = 10;
+		
+		System.out.println(d);
+	}
+
+}
