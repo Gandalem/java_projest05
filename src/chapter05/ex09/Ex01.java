2af1846b74f6b64271294d2b9d85dc295caaf880
gyumin park <rbals8429@gmail.com>
2022-11-18 09:11:51 +0900

    2022/11/18

--- src/chapter05/ex09/Ex01.java
+++ src/chapter05/ex09/Ex01.java
@@ -33,7 +33,7 @@ public class Ex01 {
 		
 		System.out.println("        국어     영어     수학     과학     <합계>     <평균>");
 		System.out.println("=====================================================");
-		for(int i=0;i<args.length;i+=5) {
+		for(int i=0;i<args.length;i+=5) { //i의변수값은 과목수+1(이름)
 			output(args,i);
 		}
 		System.out.println("=====================================================");
@@ -53,18 +53,18 @@ public class Ex01 {
 		int sum = 0;
 		int count=0;
 		for(int i=num;i<arra.length;i++) {
-			if(i==21||count==4) {
+			if(i==arra.length||count==4) { //count는 과목수 
 				break;
-			}else if(i==0||i==5||i==10||i==15){
+			}else if(i==0||i==5||i==10||i==15){ //조건값은(이름이 들어가있는 인덱스 값)
 				continue;
 			}else {
 				sum += Integer.parseInt(arra[i]);
 				count++;
 			}
 		}
-		double avg = sum/4.0;
+		double avg = sum/count;
 		System.out.println(arra[num]+"    "+arra[num+1]+"      "+arra[num+2]+"     "+arra[num+3]+"      "+arra[num+4]+"      "+sum+"      "+avg);
-		
+//		위 실행문의 arra+*를 넣어서 과목의 점수를 출력
 		return a;
 	}
 	
